6a199780a590a29b4f3a1a7efe54ca3f9bfa0cf2
Sujeet Gund <sujeetgund@gmail.com>
2022-07-14 01:20:44 +0530

    small ts bug fixed!

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,23 @@ import TopStories from '../components/TopStories'
 import { topStories } from '../typings'
 
 interface Props {
-  topStories: topStories
+  topStories: {
+    status: string
+    totalResults: number
+    articles: Array<{
+        source: {
+            id: string
+            name: string
+        },
+        author: string
+        title: string
+        description: string
+        url: string
+        urlToImage: string
+        publishedAt: string
+        content: string
+    }>
+}
 }
 
 const Home = ({ topStories }: Props) => {
